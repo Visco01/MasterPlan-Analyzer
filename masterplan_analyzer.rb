eed68fcdb99eb09b2dccee6ac5ca24d35539de00
Pietro Visconti <visconti373@gmail.com>
2022-07-28 15:20:35 +0200

    added org/html export template

--- masterplan_analyzer.rb
+++ masterplan_analyzer.rb
@@ -4,6 +4,16 @@ def main
   plan = MasterPlan.new
   plan.load_last_week
   plan.print_last_week
+ 
+  # begin
+  #   file = File.open("./resources/template.html")
+  #   file.each do |line|
+  #     puts "#{line}"
+  #   end
+  # rescue
+  #   puts "File not found."
+  #   exit(false)
+  # end
 end
 
 main
